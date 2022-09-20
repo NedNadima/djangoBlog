962c1b8006539a7a09d459c16bc993834f70ae96
Ned Nadima <nnadima1@gmail.com>
2022-09-20 19:42:44 -0400

    Added Bootstrap5

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -10,3 +10,6 @@ def index(request):
 def post(request, slug):
     print(slug)
     return render(request, 'post.html', {'post': get_object_or_404(Post, slug=slug)})
+
+def about(request):
+    return render(request, 'about.html', {})
