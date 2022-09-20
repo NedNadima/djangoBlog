1a3d97e218ef365d0434581071e94bd8b39376db
Ned Nadima <nnadima1@gmail.com>
2022-09-20 17:15:50 -0400

    Added urls and views

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
+from blog import views as blog_views
+
 
 urlpatterns = [
+    url(r'^post/$', blog_views.post),
+    url(r'^$', blog_views.index),
     path('admin/', admin.site.urls),
 ]
